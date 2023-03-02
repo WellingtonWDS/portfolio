a768deb7cced55c35a478589553405a02f2c74af
Wellington Duarte <44651361+WellingtonWDS@users.noreply.github.com>
2023-03-02 01:17:19 -0300

    feat(main) Add tilt card and personal data

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,25 +1,101 @@
 import { Component } from "react";
 import "./Main.css";
-// import profilePic from "./assets/profile-pic.png";
-import Tilt from "react-vanilla-tilt";
+import brFlag from "./assets/brazilian-flag.jpg";
+import usaFlag from "./assets/usa-flag.jpg";
+import linkedinLogo from "./assets/linkedin-logo.png";
+import githubLogo from "./assets/github-logo.png";
+import downloadLogo from "./assets/download-logo.png";
+
+import Tilt from "react-parallax-tilt";
 
 class Main extends Component {
   render() {
     return (
       <section className="main">
         <div className="greetings">
-          <p>Hello, I'm</p>
+          <p className="greetings-phrase">Hello, I'm</p>
           <div class="glitch-wrapper">
             <div class="glitch" data-glitch="Wellington Duarte">
               Wellington Duarte
             </div>
           </div>
-          <p>Frontend Web Developer</p>
+          <p className="greetings-ocupation">Web Developer</p>
         </div>
         <div className="figure">
-          <Tilt id="box" options={{ scale: 1, max: 25, glare: true }}>
-            <div className="box">
-              <div className="elements imgBox"></div>
+          <Tilt
+            className="box"
+            style={{
+              height: "400px",
+              width: "400px",
+              borderRadius: "10px",
+            }}
+            tiltAngleYInitial="30"
+            perspective={500}
+          >
+            <div className="profile-pic"></div>
+            <div className="squares left-square">
+              <img src={brFlag} alt="Brazilian flag" className="flags" />
+              <p>Portuguese Fluency</p>
+              <p>Native</p>
+            </div>
+            <div className="squares right-square">
+              <img src={usaFlag} alt="Brazilian flag" className="flags" />
+
+              <p>English Fluency</p>
+              <p>C2 - Proficient</p>
+              <a
+                href="https://www.efset.org/cert/pNjhUa"
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                Certificate Link
+              </a>
+            </div>
+            <div className="squares  card-infos">
+              <p>Brazilian</p>
+              <p>Born in Campinas - SP</p>
+              <p>Living in Araçuaí - MG</p>
+            </div>
+
+            {/* Contact */}
+            <div className="contact-squares">
+              <a
+                href="https://www.linkedin.com/in/wellington-duarte-santos-934ba61aa/"
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                <div className="squares linkedin">
+                  <img
+                    src={linkedinLogo}
+                    alt="LinkedIn Logo"
+                    className="contact-logo"
+                  />
+                  <p>LinkedIn</p>
+                </div>
+              </a>
+
+              <a
+                href="https://github.com/WellingtonWDS"
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                <div className="squares github">
+                  <img
+                    src={githubLogo}
+                    alt="Github Logo"
+                    className="contact-logo"
+                  />
+                  <p>Github</p>
+                </div>
+              </a>
+              <div className="squares cv">
+                <img
+                  src={downloadLogo}
+                  alt="Download Logo"
+                  className="contact-logo"
+                />
+                <p>Download CV</p>
+              </div>
             </div>
           </Tilt>
 
